96bc65454916d089608c23991e9c891d327af59f
Abdulrahman Al-Kiswany <a.alkiswany@gtt.llc>
2023-08-03 00:53:26 +0300

    simple swagger fix

--- School-Administration/Startup.cs
+++ School-Administration/Startup.cs
@@ -37,14 +37,32 @@ namespace School_Administration
             services.AddDbContext<SchoolDbContext>(item => item.UseSqlServer(Configuration.GetConnectionString("MyConnection")));
             services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
             services.AddSwaggerGen();
-            services.AddSwaggerGen(c =>
-            {
+            services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
-                    Version = "v1",
-                    Title = "Implement Swagger UI",
-                    Description = "A simple example to Implement Swagger UI",
+                    Title = "JWTToken_Auth_API",
+                    Version = "v1"
                 });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
             });
 
             services.AddAuthorization(config =>
